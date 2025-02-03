ddaf5a9fe883468296e288c209910537b67d0427
m-araf <lentoaraf@gmail.com>
2025-02-04 02:50:05 +0500

    Even odd with for loop

---
+++ for-loop.js
@@ -0,0 +1,9 @@
+let i;
+for(i=1;i<=10;i++){
+    if(i%2==0){
+        console.log(i + " Is Even");
+    }else{
+        console.log(i+" Is Odd");
+    }
+}
+console.log("Pani mein gaya.. chappak!");
