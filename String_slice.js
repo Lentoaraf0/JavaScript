cf4ce234e278fa59a4aa95f140d75d53bc40e3ab
m-araf <lentoaraf@gmail.com>
2025-02-06 13:05:21 +0500

    String Slice

---
+++ String_slice.js
@@ -0,0 +1,7 @@
+let text = "JavaScript is very hard";
+let textslice1 = text.slice(0,10);
+let textslice2 = text.slice(4,10);
+let textslice3 = text.slice(19,23);
+console.log(textslice1);
+console.log(textslice2);
+console.log(textslice3);
