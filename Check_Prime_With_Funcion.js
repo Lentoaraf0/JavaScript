416b242a1f4810115bafdfc9681840d2accb195f
m-araf <lentoaraf@gmail.com>
2025-02-13 04:42:31 +0500

    Check prime or not with function

---
+++ Check_Prime_With_Funcion.js
@@ -0,0 +1,8 @@
+function Prime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= num-1; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+console.log(Prime(4));
